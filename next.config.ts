28fd89fb5f475d7ae7c0a9551fd0598c44b7b174
harshavardanan <harshamoorthy22@gmail.com>
2025-01-27 21:37:02 +0000

    final build commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
+  assetPrefix: "./",
+  trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
