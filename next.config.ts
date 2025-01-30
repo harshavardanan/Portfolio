e62cc7dc070e9b8322bc1a18c8a9c03ac54dd455
harshavardanan <harshamoorthy22@gmail.com>
2025-01-30 19:13:45 +0000

    images fixed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "",// while pushing to github pages, set this to "/<REPO_NAME>"
+  basePath: "", // while pushing to github pages, set this to "/<REPO_NAME>"
   trailingSlash: true,
   images: {
     unoptimized: true,
